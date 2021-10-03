05643a81ce9416a674b6dd80b34b904c52d76a4d
Jh0n-0000 <susanoo24jhon123@gmail.com>
2021-10-03 13:35:12 -0400

    MCD Recursividad

---
+++ src/Ejercicios.java
@@ -0,0 +1,47 @@
+public class Ejercicios {
+
+    /*Factorial de un numero
+    5! = 5 * 4 * 3 * 2 * 1
+    4! = 4 * 3 * 2 * 1
+
+     */
+    public static int factorial(int n) {
+        int factorial;
+
+        if (n == 1){
+            //caso base
+            factorial = n;
+
+        }else {
+            factorial = n * factorial(n - 1);
+        }
+
+        return factorial;
+    }
+
+    /*
+    Maximo comun divisor (mcd), algoritmo de euclides
+    a = 412 y b = 184
+    */
+    public static int mcd(int a, int b) {
+        int mcd;
+
+        if(a == b){
+            //caso base
+            mcd = b;
+
+        }else{
+            if (a > b) {
+                a = a - b;
+
+            } else {
+                b = b - a;
+            }
+
+            mcd = mcd(a , b);
+
+        }
+
+        return mcd;
+    }
+}
